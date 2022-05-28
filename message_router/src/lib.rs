e052fc328119fc777cc8f41de7ff138513d787a0
StefanMallia <5004438+StefanMallia@users.noreply.github.com>
2022-05-28 21:11:59 +0200

    Respond to requester when error.
    Requester receives response when message cannot be delivered to the
    replier. An example of this is when the replier sends a message before
    the replier can register its identity with the router.

--- message_router/src/lib.rs
+++ message_router/src/lib.rs
@@ -12,6 +12,7 @@ impl MessageRouter
         let ctx = zmq::Context::new();
 
         let socket = ctx.socket(zmq::ROUTER).unwrap();
+        socket.set_router_mandatory(true);
         socket.bind(connection_string).unwrap();
         let socket = Arc::new(futures::lock::Mutex::new(socket));
         MessageRouter{socket}
@@ -30,7 +31,22 @@ impl MessageRouter
             message[0] = dest_address;
             message[2] = source_address;
 
-            socket.send_multipart(message, 0).unwrap();
+            
+
+            let result = socket.send_multipart(&message, 0);
+            match result
+            {
+                Ok(res) => res,
+                Err(err) =>
+                {
+                    let destination = message[0].clone();
+                    message[0] = message[2].clone();
+                    message[2] = destination;
+                    message[4] = err.to_string().as_bytes().to_vec();
+                    socket.send_multipart(&message, 0);
+
+                }
+            }
         }    
     }
 }
